f7f969422972f08cedec7dabe9b3d5bb366bfc83
kiratopat <capacity1412@gmail.com>
2024-04-05 18:01:30 +0700

    modify home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,120 +1,107 @@
 "use client";
 import Nav from "@/components/Nav";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
+
+import MenuCard from "@/components/MenuCard";
+import { debounce } from "lodash";
+import Typewriter from "typewriter-effect";
+import { useState, useEffect, useCallback } from "react";
+import { Food } from "@/interface/recipes";
 
 export default function Home() {
   const { data: session } = useSession();
+  const food: Food[] = [
+    {
+      id: 1,
+      name: "กระเพราหมูสับ",
+      rating: 4.5,
+      src: "/images/recipes/reciepe_1.webp",
+    },
+    {
+      id: 2,
+      name: "แพนงหมู",
+      rating: 4.6,
+      src: "/images/recipes/reciepe_1.webp",
+    },
+    {
+      id: 3,
+      name: "ต้มยำกุ้ง",
+      rating: 4.1,
+      src: "/images/recipes/reciepe_1.webp",
+    },
+  ];
+
+  const [searchKey, setsearchKey] = useState<string>(``);
+  const [filteredFood, setFilteredFood] = useState<Food[]>(food);
+
+  const debouncedSave = useCallback(
+    debounce((nextValue) => setFilteredFood(nextValue), 500),
+    []
+  );
+
+  useEffect(() => {
+    if (searchKey === "") {
+      debouncedSave(food);
+    } else if (searchKey) {
+      debouncedSave(
+        food.filter((item) => item.name.toLowerCase().includes(searchKey))
+      );
+    }
+  }, [searchKey, debouncedSave]);
+
   return (
     <>
       <Nav session={session} />
-
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Get started by editing&nbsp;
-            <code className="font-mono font-bold">src/app/page.tsx</code>
-          </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
+        <section className="flex flex-col text-center gap-4" id="yop-search">
+          {/* <h1>What menu is on your mind ?</h1> */}
+          <div className="h-28 md:h-auto">
+            <span className="text-4xl md:text-6xl font-bold drop-shadow-md drop-shadow-white">
+              <Typewriter
+                options={{
+                  strings: [
+                    "What menu is on your mind ?",
+                    "What is your favorite food ?",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
               />
-            </a>
+            </span>
           </div>
-        </div>
-
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Docs{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Learn{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Templates{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Explore starter templates for Next.js.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Deploy{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div className="flex w-full justify-center">
+            {/* <div className="flex gap-8 mr-10"> */}
+            <div className="form-control flex flex-row justify-center gap-2 w-[90vw]">
+              <input
+                type="text"
+                placeholder="Search"
+                className="input input-bordered w-full  self-center bg-slate-300 text-black"
+                value={searchKey}
+                onChange={(e) => {
+                  setsearchKey(e.target.value);
+                }}
+              />
+              {/* <button
+                onClick={() => {}}
+                className="btn bg-orange-800 text-white"
+              >
+                Search
+              </button> */}
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="flex flex-wrap w-full gap-4 mt-4">
+            {filteredFood.map((item: Food) => (
+              <MenuCard
+                // session={session}
+                menuName={item.name}
+                menuId={item.id}
+                src={item.src}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </section>
       </main>
     </>
   );
