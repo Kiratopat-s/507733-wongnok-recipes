f6fc1c55a1c7064a79c9234038c0ef9c08cc647c
kiratopat <capacity1412@gmail.com>
2024-04-06 17:22:56 +0700

    adjust a user display no navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,11 +3,12 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 import { DefaultSession } from "next-auth";
 import Link from "next/link";
+import Image from "next/image";
 
 function Nav({ session }: { session?: DefaultSession | null }) {
   return (
-    <nav className="z-50 fixed top-0 w-full p-4 bg-black text-white flex justify-between  rounded-b-lg ring-1 ring-white shadow shadow-white">
-      <div className="flex justify-start item-center align-middle ml-10">
+    <nav className="z-50 fixed top-0 w-full p-4 bg-black text-white flex justify-around  rounded-b-lg ring-1 ring-white shadow shadow-white">
+      <div className="flex justify-start item-center align-middle">
         <Link
           className="flex flex-col justify-center hover:text-rose-500 hover:drop-shadow-xl text-2xl font-bold drop-shadow drop-shadow-white cursor-pointer transition-all duration-300 ease-in-out"
           href={"/"}
@@ -15,26 +16,39 @@ function Nav({ session }: { session?: DefaultSession | null }) {
           Wongnok 🍲
         </Link>
       </div>
-      <div className="flex gap-8 justify-end mr-10">
+      <div className="flex gap-4 md:gap-8 justify-end mr-0 md:mr-10">
         {session && (
           <>
-            <div className="flex gap-4">
-              <p className="btn">Welcome {session.user?.name} !</p>
-              <div className="avatar online">
-                <div className="w-12 rounded-full">
-                  <img
+            <div className="flex gap:2 md:gap-4">
+              <p className="btn hidden md:flex">
+                Welcome {session.user?.name} !
+              </p>
+              <div className="avatar">
+                <div className="w-10 h-10 md:w-12 md:h-12 rounded-full self-center">
+                  <Image
                     src={
                       session.user?.image ||
                       `https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg`
                     }
+                    alt={`user avatar`}
+                    width={256}
+                    height={256}
+                    className={`self-center h-5 object-cover rounded-lg`}
+                    loading="lazy"
                   />
+                  {/* <img
+                    src={
+                      session.user?.image ||
+                      `https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg`
+                    }
+                  /> */}
                 </div>
               </div>
             </div>
 
             <button
               onClick={() => signOut()}
-              className="btn bg-red-500 text-white"
+              className="btn bg-red-500 text-white scale-[90%] md:scale-100"
             >
               Logout
             </button>
