a82a07875c4cf813680aeb9d4d6df460d09dd6ed
kiratopat <capacity1412@gmail.com>
2024-04-08 00:39:29 +0700

    remove Line provider and fix many authen include prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 import LineProvider from "next-auth/providers/line";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 const secret = process.env.NEXTAUTH_SECRET
 export const authOptions = {
     providers: [
@@ -9,22 +11,47 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
 
         }),
-        LineProvider({
-            clientId: process.env.LINE_CLIENT_ID || '',
-            clientSecret: process.env.LINE_CLIENT_SECRET || ''
-        })
     ],
     callbacks: {
-        async jwt({ token, account }: any) {
-            if (account) {
-                token.accessToken = account.access_token
+        authorized({ auth, request: { nextUrl } }: { auth: { user: any }, request: { nextUrl: any } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
             }
-            return token
+            return true;
         },
-        async session({ session, token, user }: any) {
-            session.accessToken = token.accessToken
-            return session
-        }
-    }
+        async jwt({ token, user, account }: { token: any, user: any, account: any }) {
+            if (account) {
+                token.id_token = account.id_token;
+
+                // Check if accessToken exists in the Member model
+                const member = await prisma.member.findUnique({
+                    where: {
+                        email: user.email,
+                    },
+                });
+
+                // If member doesn't exist, insert a new member
+                if (!member) {
+                    await prisma.member.create({
+                        data: {
+                            name: user.name,
+                            email: user.email,
+                            image_url: user.image,
+                        },
+                    });
+                }
+            }
+            return token;
+        },
+        async session({ session, token }: { session: any, token: any }) {
+            session.id_token = token.id_token;
+            return session;
+        },
+    },
 };
 export default NextAuth(authOptions)
