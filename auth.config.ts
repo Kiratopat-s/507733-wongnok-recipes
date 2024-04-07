8177cc5460d8c2f6728efabc8fd882c446d9be8e
kiratopat <capacity1412@gmail.com>
2024-04-07 22:52:04 +0700

    get accessToken from authentication

--- auth.config.ts
+++ auth.config.ts
@@ -17,6 +17,17 @@ export const authConfig: any = {
             }
             return true;
         },
+        async jwt({ token, user, account }: { token: any, user: any, account: any }) {
+
+            if (account) {
+                token.id_token = account.id_token;
+            }
+            return token;
+        },
+        async session({ session, token }: { session: any, token: any }) {
+            session.id_token = token.id_token;
+            return session;
+        },
     },
     providers: [], // Add providers with an empty array for now
 } satisfies any;
