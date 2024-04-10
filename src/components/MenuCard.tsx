5d6e3cd8a22d709621a1e51f9ae65a723d8faad4
kiratopat <capacity1412@gmail.com>
2024-04-10 21:39:24 +0700

    modify component

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -5,27 +5,67 @@ import { DefaultSession } from "next-auth";
 import Link from "next/link";
 
 function MenuCard({
-  session,
   menuName,
   menuId,
   src,
   rating,
+  isOwner,
+  isHome,
+  hh = 0,
+  mm = 0,
+  difficulty = "Loading...",
 }: {
   session?: DefaultSession | null;
   menuName: string;
   menuId: number;
   src: string;
   rating: number;
+  isOwner: boolean;
+  isHome: boolean;
+  hh?: number;
+  mm?: number;
+  difficulty?: string;
 }) {
   const [loadingStatus, setloadingStatus] = useState<boolean>(true);
-  // console.log(menuName);
   return (
-    // <div className="flex w-full">
-    <Link key={menuId} className="relative w-full" href={`/recipes/${menuId}`}>
-      <p className="absolute top-0 px-4 py-2 text-center md:text-2xl bg-black rounded-tl rounded-br">
+    <Link
+      key={menuId}
+      className="relative rounded-md w-full overflow-hidden"
+      href={`/recipes/${menuId}`}
+    >
+      <p className="z-20 absolute top-0 px-4 py-2 text-center md:text-2xl bg-black rounded-tl rounded-br">
         {menuName}
       </p>
-      <p className="absolute right-0 px-4 py-2 text-lg md:text-2xl text-center bg-black rounded-bl rounded-tr">
+      {isOwner && (
+        <Link
+          href={`/recipes/edit/${menuId}`}
+          className="z-20 absolute bottom-0 left-0 px-4 py-2 text-center md:text-2xl bg-orange-500 hover:bg-gray-800 transition-all duration-300 ease-in-out text-white rounded-bl rounded-tr"
+        >
+          <i className="fa-solid fa-pen-to-square mr-2"></i>Edit
+        </Link>
+      )}
+      {isHome && (
+        <>
+          <p className="z-20 absolute right-0 bottom-0 px-4 py-0.5 text-lg md:text-lg text-center bg-black rounded-bl rounded-tr">
+            <i className="fa-solid fa-clock mr-2 text-md text-white"></i>
+            {hh} h {mm} m
+          </p>
+          <p
+            className={`z-20 absolute left-0 bottom-0 px-4 py-0.5 text-lg md:text-lg text-center bg-black bg-blur-xl ${
+              difficulty === "Hard"
+                ? `text-red-500`
+                : difficulty === "Normal"
+                ? `text-orange-500`
+                : difficulty === "Easy"
+                ? `text-green-500`
+                : `text-purple-500`
+            } rounded-bl rounded-tr`}
+          >
+            {difficulty}
+          </p>
+        </>
+      )}
+      <p className="z-20 absolute right-0 top-0 px-4 py-2 text-lg md:text-2xl text-center bg-black rounded-bl rounded-tr">
         {rating} <span className=" text-gray-500">/5 </span>⭐
       </p>
       {/* {session && (
@@ -37,12 +77,12 @@ function MenuCard({
         {/* {loadingStatus && <div className="skeleton w-960 h-40"></div>} */}
 
         <Image
-          src={src}
+          src={`/images/recipes/${src}`}
           alt={`image for menu ${menuName}`}
           width={1024}
           height={1024}
-          className={` w-full h-40 object-cover rounded-lg`}
-          loading="lazy"
+          className={` w-full h-40 object-cover rounded-lg scale-100 hover:scale-[110%] brightness-[90%] md:brightness-[80%] hover:brightness-[100%] transition-all duration-300 ease-in-out`}
+          priority={true}
         />
       </div>
     </Link>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,18 +4,21 @@ import React from "react";
 import { DefaultSession } from "next-auth";
 import Link from "next/link";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 function Nav() {
   const { data: session } = useSession();
-  console.log(session);
   return (
     <nav className="z-50 fixed top-0 w-full p-4 bg-black text-white flex justify-around  rounded-b-lg ring-1 ring-white shadow shadow-white">
+      <Toaster position="top-right" reverseOrder={false} />
       <div className="flex justify-start item-center align-middle">
         <Link
           className="flex flex-col justify-center hover:text-rose-500 hover:drop-shadow-xl text-2xl font-bold drop-shadow drop-shadow-white cursor-pointer transition-all duration-300 ease-in-out"
           href={"/"}
         >
-          Wongnok 🍲
+          <span>
+            <i className="fa-solid fa-kitchen-set mr-"></i> Wongnok
+          </span>
         </Link>
       </div>
       <div className="flex gap-4 md:gap-8 justify-end mr-0 md:mr-10">
@@ -24,13 +27,13 @@ function Nav() {
             <div className="flex gap:2 md:gap-4">
               <Link
                 href={"/recipes/newRecipe"}
-                className="btn shadow-lg btn-primary text-white font-bold py-3 rounded-2xl bg-orange-500 md:flex hidden"
+                className="btn shadow-lg btn-primary text-white font-bold py-3 bg-orange-500 md:flex hidden"
               >
-                📝 New post
+                <i className="fa-solid fa-file-pen "></i>New post
               </Link>
-              <p className="btn hidden md:flex">
-                Welcome {session.user?.name} !
-              </p>
+              <Link href={"/recipes/mine"} className="btn hidden md:flex">
+                <i className="fa-solid fa-book"></i>My recipes
+              </Link>
               <div className="avatar">
                 <Link
                   href={"/recipes/mine"}
@@ -55,7 +58,7 @@ function Nav() {
               onClick={() => signOut()}
               className="btn bg-red-500 text-white scale-[90%] md:scale-100"
             >
-              Logout
+              <i className="fa-solid fa-right-from-bracket"></i>Logout
             </button>
           </>
         )}
